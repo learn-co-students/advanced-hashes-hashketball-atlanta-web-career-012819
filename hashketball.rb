1f54dda1834a0a2ff9ea736432bd82b2e7cc20d4
Leigh Baugham <leigh.baugham@gmail.com>
2019-01-19 12:28:54 -0500

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code here!
+require "pry"
 def game_hash
    {
  :home => {
@@ -7,54 +8,54 @@ def game_hash
    :players => [
      {
       :player_name => "Alan Anderson",
-      :number => "0",
-      :shoe => "16",
-      :points => "22",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "3",
-      :blocks => "1",
-      :slam_dunks => "1"
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
    }, {
       :player_name => "Reggie Evans",
-      :number => "30",
-      :shoe => "14",
-      :points => "12",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "12",
-      :blocks => "12",
-      :slam_dunks => "7"
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
    }, {
       :player_name => "Brook Lopez",
-      :number => "11",
-      :shoe => "17",
-      :points => "17",
-      :rebounds => "19",
-      :assists => "10",
-      :steals => "3",
-      :blocks => "1",
-      :slam_dunks => "15"
+      :number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 15
    }, {
      :player_name => "Mason Plumlee",
-      :number => "1",
-      :shoe => "19",
-      :points => "26",
-      :rebounds => "12",
-      :assists => "6",
-      :steals => "3",
-      :blocks => "8",
-      :slam_dunks => "5"
+      :number => 1,
+      :shoe => 19,
+      :points => 26,
+      :rebounds => 12,
+      :assists => 6,
+      :steals => 3,
+      :blocks => 8,
+      :slam_dunks => 5
    }, {
      :player_name => "Jason Terry",
-      :number => "31",
-      :shoe => "15",
-      :points => "19",
-      :rebounds => "2",
-      :assists => "2",
-      :steals => "4",
-      :blocks => "11",
-      :slam_dunks => "1"
+      :number => 31,
+      :shoe => 15,
+      :points => 19,
+      :rebounds => 2,
+      :assists => 2,
+      :steals => 4,
+      :blocks => 11,
+      :slam_dunks => 1
     }
    ]
  },
@@ -65,54 +66,54 @@ def game_hash
    :players => [
      {
        :player_name => "Jeff Adrien",
-      :number => "4",
-      :shoe => "18",
-      :points => "10",
-      :rebounds => "1",
-      :assists => "1",
-      :steals => "2",
-      :blocks => "7",
-      :slam_dunks => "2"
+      :number => 4,
+      :shoe => 18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2
      }, {
        :player_name => "Bismak Biyombo",
-      :number => "0",
-      :shoe => "16",
-      :points => "12",
-      :rebounds => "4",
-      :assists => "7",
-      :steals => "7",
-      :blocks => "15",
-      :slam_dunks => "10"
+      :number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 7,
+      :blocks => 15,
+      :slam_dunks => 10
      }, {
        :player_name => "DeSagna Diop",
-      :number => "2",
-      :shoe => "14",
-      :points => "24",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "4",
-      :blocks => "5",
-      :slam_dunks => "5"
+      :number => 2,
+      :shoe => 14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
      }, {
        :player_name => "Ben Gordon",
-      :number => "8",
-      :shoe => "15",
-      :points => "33",
-      :rebounds => "3",
-      :assists => "2",
-      :steals => "1",
-      :blocks => "1",
-      :slam_dunks => "0"
+      :number => 8,
+      :shoe => 15,
+      :points => 33,
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
+      :slam_dunks => 0
      }, {
        :player_name => "Brendan Haywood",
-      :number => "33",
-      :shoe => "15",
-      :points => "6",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "22",
-      :blocks => "5",
-      :slam_dunks => "12"
+      :number => 33,
+      :shoe => 15,
+      :points => 6,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 22,
+      :blocks => 5,
+      :slam_dunks => 12
      }
     ]
   }
@@ -123,7 +124,7 @@ def num_points_scored(player_name)
   game_hash.each do |team, team_data|
   team_data[:players].each do |player|
       if player[:player_name] == player_name
-        return player[:points].to_i
+        return player[:points]
       end
     end
 end
@@ -133,7 +134,7 @@ def shoe_size(player_name)
   game_hash.each do |team, team_data|
   team_data[:players].each do |player|
       if player[:player_name] == player_name
-        return player[:shoe].to_i
+        return player[:shoe]
       end
     end
 end
@@ -167,11 +168,11 @@ end
 def player_numbers(team_name)
 player_numbers_array = []
   game_hash.each do |team, team_data|
-    if team_data[:name] == team_name
+    if team_data[:team_name] == team_name
       team_data[:players].each do |player|
         player.each do |key, value|
           if key == :number
-            player_numbers_array << value
+            player_numbers_array << value.to_i
           end
         end
       end
@@ -181,4 +182,30 @@ player_numbers_array = []
 end
 
 
-  
+def player_stats(player_name)
+  player_stats = {}
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |stats|
+
+      if stats[:player_name] == player_name
+        stats.delete(:player_name)
+        player_stats = stats
+      end
+    end
+  end
+player_stats
+end
+
+def big_shoe_rebounds
+  big_shoes = 0
+  rebounds = 0
+    game_hash.each do | team, team_data|
+      team_data[:players].each do | stats |
+        if stats[:shoe] > big_shoes
+          big_shoes = stats[:shoe]
+          rebounds = stats[:rebounds]
+        end
+      end
+    end
+  rebounds
+end
